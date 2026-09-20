93505bd7c29cd2526273fe035217586bd83a8529
nssmsbot <avaxopoze23@gmail.com>
2026-09-20 19:00:50 +0600

    Update and rename bot_fixed_no_reuse_with_prefix_railway.py to  bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ from aiogram.exceptions import TelegramBadRequest
 from aiogram.dispatcher.event.bases import SkipHandler
 
 # ================= CONFIGURATION ==================
-BOT_TOKEN = "8903349571:AAFJTyQJ1IQc-7HdXo95HrwTpYUF9c4IaPU"
+BOT_TOKEN        = "8603037987:AAEAt4Ilcgn1WcrjmVCC5ythBA9kXvJjIJE"
 CHANNEL_ID       = "@nonstopxearn"
 OTP_GROUP        = "@nssmsotp"
 FORWARD_GROUP_ID = 1004474161960
