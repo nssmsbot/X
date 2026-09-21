23e55c065eddfeb30f8fb0955f45763e1d5908b1
nssmsbot <avaxopoze23@gmail.com>
2026-09-21 08:10:10 +0600

    Change bot token in bot.py
    Updated the bot token for authentication.

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ from aiogram.exceptions import TelegramBadRequest
 from aiogram.dispatcher.event.bases import SkipHandler
 
 # ================= CONFIGURATION ==================
-BOT_TOKEN        = "8903349571:AAFz_wnoY0cBhHD-A5ead8_U418z0I_ZwiE"
+BOT_TOKEN        = "8892340625:AAEhdvhDyKvruFNlW_cUFsW48Va_gNQU3UI"
 CHANNEL_ID       = "@nonstopxearn"
 OTP_GROUP        = "@nssmsotp"
 FORWARD_GROUP_ID = 1004474161960
