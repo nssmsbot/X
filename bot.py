bf9843c23bc28134ae4d96f679ca7ec4683ac620
nssmsbot <avaxopoze23@gmail.com>
2026-09-25 11:15:47 +0600

    Update and rename bot%20%281%29.py to bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ from aiogram.exceptions import TelegramBadRequest
 from aiogram.dispatcher.event.bases import SkipHandler
 
 # ================= CONFIGURATION ==================
-BOT_TOKEN        = "xxxxxxg"
+BOT_TOKEN        = "8892340625:AAGUsNtgaPRXlGqrz5JdKQD-F00jpXunSDU"
 CHANNEL_ID       = "@nonstopxearn"
 OTP_GROUP        = "@nssmsotp"
 FORWARD_GROUP_ID = 1004474161960
