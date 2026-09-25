540c2fa843f537efb4efb055cab72c20b4eeaee9
nssmsbot <avaxopoze23@gmail.com>
2026-09-25 15:00:25 +0600

    Delete bot.py
    Updated the BOT_TOKEN with a new value.

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ from aiogram.exceptions import TelegramBadRequest
 from aiogram.dispatcher.event.bases import SkipHandler
 
 # ================= CONFIGURATION ==================
-BOT_TOKEN        = "xxxxxxg"
+BOT_TOKEN        = "8892340625:AAFb8kFn_4lIheJR6sXSK9WbvK67W61NwpE"
 CHANNEL_ID       = "@nonstopxearn"
 OTP_GROUP        = "@nssmsotp"
 FORWARD_GROUP_ID = 1004474161960
